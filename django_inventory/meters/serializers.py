edcab0d1f27c5580e29e60629f97b57636807443
jslv <jslv@macbook-air.local>
2024-02-01 11:21:50 +0300

    meters: create, update

--- django_inventory/meters/serializers.py
+++ django_inventory/meters/serializers.py
@@ -8,13 +8,29 @@ class MeterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Meter
-        fields = '__all__'
+        fields = ('__all__')
 
     def get_verification_date(self, obj):
         verification_entry = MeterVerification.objects.filter(meter=obj).order_by('-created_at').first()
         if verification_entry:
-            return verification_entry['date']
+            return verification_entry.date
         return None
+
+
+class MeterCreateSerializer(serializers.ModelSerializer):
+    verification_date = serializers.DateField(required=False)
+
+    class Meta:
+        model = Meter
+        fields = ('__all__')
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['verification_date'] = None
+        verification_entry = MeterVerification.objects.filter(meter=instance).order_by('-created_at').first()
+        if verification_entry:
+            representation['verification_date'] = verification_entry.date
+        return representation
 
     def create(self, validated_data):
         verification_date = validated_data.pop('verification_date')
@@ -22,3 +38,18 @@ class MeterSerializer(serializers.ModelSerializer):
         if verification_date is not None:
             MeterVerification.objects.create(meter=meter, date=verification_date)
         return meter
+
+
+class MeterUpdateSerializer(serializers.ModelSerializer):
+    verification_date = serializers.DateField(required=False)
+
+    class Meta:
+        model = Meter
+        exclude = ('uuid', 'created_at', 'updated_at')
+
+    def update(self, instance, validated_data):
+        verification_date = validated_data.pop('verification_date')
+        if verification_date:
+            MeterVerification.objects.create(meter=instance, date=verification_date)
+        super().update(instance=instance, validated_data=validated_data)
+        return instance
--- django_inventory/meters/views.py
+++ django_inventory/meters/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 
-from .serializers import MeterSerializer
+from .serializers import MeterSerializer, MeterCreateSerializer, MeterUpdateSerializer
 from .models import Meter
 
 
@@ -8,4 +8,8 @@ class MeterViewSet(viewsets.ModelViewSet):
     queryset = Meter.objects.all()
 
     def get_serializer_class(self):
+        if self.action in ('update', 'partial_update'):
+            return MeterUpdateSerializer
+        elif self.action in ('create', ):
+            return MeterCreateSerializer
         return MeterSerializer
