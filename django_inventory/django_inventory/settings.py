2559256c90e23f354ac0a2c53d2b475415ebecdd
jslv <jslv@macbook-air.local>
2024-02-01 12:51:16 +0300

    dotenv

--- django_inventory/django_inventory/settings.py
+++ django_inventory/django_inventory/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!a7(g8&br2#!+tbh&p_=#2ztk4co)k2lz&y&!_l(es%zg(eop6"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
 
 # Application definition
 
--- django_inventory/manage.py
+++ django_inventory/manage.py
@@ -3,8 +3,11 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
 
 def main():
+    load_dotenv('.env')
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_inventory.settings")
     try:
